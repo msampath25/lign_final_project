e22d017ebb5968cc86a967dfe98b5c3086e9d81e
msampath25 <m25sampath@gmail.com>
2024-11-21 22:42:03 -0800

    Writing code for course catalog data and creating a template vue component

---
+++ preprocessing/code/course_catalog.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+courses = ['LING']
+for c in courses:
+    html_file = requests.get(f"https://catalog.ucsd.edu/courses/{c}.html")
+    f = open(f'../{c}_catalog.txt', 'w')
+    for line in html_file.iter_lines():
+        line = line.decode('utf-8')
+        print(line)
+        if "course-name" in line:
+            pattern = r'"course-name">(.*?)</p>'
+            match = re.search(pattern, line)
+            if match:
+                f.write('>' + match.group(1) + '\n')
+        elif "course-descriptions" in line:
+            pattern = r'course-descriptions">(.*?)<strong.*?Prerequisites:</em></strong>(.*?)</p>'
+            match = re.search(pattern, line)
+            if match:
+                f.write(match.group(1) + '\n')
+    f.close()
