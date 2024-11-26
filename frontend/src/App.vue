5968b8566bd7778d0af0b9daa32923bfbfff7306
msampath25 <m25sampath@gmail.com>
2024-11-25 20:01:08 -0800

    Components can now pass information to Parent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import CourseLevel from './components/CourseLevel.vue';
+import Department from './components/Department.vue';
+import Prompt from './components/Prompt.vue';
 </script>
 
 <template>
   <CourseLevel />
+  <Department />
+  <Prompt />
 </template>
